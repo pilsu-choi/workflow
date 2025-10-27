94afd62850c38d1b179eb302cff13a19ec1c8ec6
pilsu-choi <chlvlftn22@gmail.com>
2025-10-27 11:17:01 +0900

    edge db field add

--- database/graph/edge.py
+++ database/graph/edge.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 from datetime import datetime, timezone
+from typing import TYPE_CHECKING
 
 from sqlalchemy import JSON, DateTime
 from sqlmodel import Field, SQLModel
+
+if TYPE_CHECKING:
+    from helpers.node.node_base import NodeInputOutput
 
 
 class Edge(SQLModel, table=True):  # type: ignore
@@ -15,7 +19,16 @@ class Edge(SQLModel, table=True):  # type: ignore
     source_id: int = Field(foreign_key="vertices.id")
     target_id: int = Field(foreign_key="vertices.id")
     type: str = Field(default=None)
-    properties: dict = Field(default_factory=dict, sa_type=JSON)
+    source_properties: "NodeInputOutput" = Field(
+        default_factory=dict,
+        sa_type=JSON,
+        description="Source Node의 output 포트",
+    )
+    target_properties: "NodeInputOutput" = Field(
+        default_factory=dict,
+        sa_type=JSON,
+        description="Target Node의 input 포트",
+    )
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
         sa_type=DateTime(timezone=True),
